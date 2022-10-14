d17c10041c8b4653973e16aa29bc167a920365e0
Wesam56 <wesammahboub@gmail.com>
2022-10-14 16:17:50 -0700

    f

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,35 @@
+#include <stdio.h>                                                                                                                                     
+#include <stdlib.h>                                                                                                                           
+#include <time.h>                                                                                                                                      
+/**                                                                                                                                                    
+ * main - Entry point                                                                                                                                  
+ *                                                                                                                                       
+ * Return: Always 0 (Success)                                                                                                                          
+ *
+ */                                                                                                                                                    
+
+int main(void)                                                                                                                                         
+
+{                                                                                                                                                      
+
+	        char c;     
+
+                char b;		
+
+		                                                                                                                                   		        c = 'a';
+	                b = 'A';		
+
+			        while                                                                                                                                      (c <= 'z') {                                                                                                                           
+ putchar(c);
+ c++;                                                    
+			  	}
+while 
+  (c <= 'z')
+  {
+putchar(c);
+b++;
+  }
+														                             
+return (0);                                   
+}		
+							                            
