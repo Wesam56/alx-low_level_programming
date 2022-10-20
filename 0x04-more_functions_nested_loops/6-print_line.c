56aa760db9a82682ce3f8f10a0c951fdbd2a0288
Wesam56 <113597907+Wesam56@users.noreply.github.com>
2022-10-21 02:19:10 +0400

    Add files via upload

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * print_line - a function that draws a straight line in the terminal
+ * @n: input number of times to print '_'
+ * Return: a straight line
+ */
+void print_line(int n)
+{
+	int x;
+
+	if (n > 0)
+	{
+		for (x = 1; x <= n; x++)
+		{
+			_putchar(95);
+		}
+	}
+	_putchar('\n');
+}
