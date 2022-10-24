046b1a2f699e07e2b198dd6331f50d4771ae86e5
Wesam56 <113597907+Wesam56@users.noreply.github.com>
2022-10-25 00:19:31 +0400

    Add files via upload

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swap to integer values
+ *
+ * @a: first value to swap
+ * @b: second value to swap
+ *
+ * Return: always void
+ */
+void swap_int(int *a, int *b)
+{
+	int n;
+
+	temp = *a;
+	*a = *b;
+	*b = n;
+}
