398279860ca140894a230623226642b742093336
Wesam56 <113597907+Wesam56@users.noreply.github.com>
2022-10-28 03:24:24 +0400

    hhh

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
- * _strcat - concatenates two strings
+ * _strncpy - copies a string to another string
  *
- * @dest: the string @src is added to
- * @src: the string to be appended
+ * @dest: the buffer
+ * @src: the string to be copied
+ * @n: the number of bytes to include in @dest
  *
  * Return: pointer to the resulting string @dest
  */
 
-char *_strcat(char *dest, char *src)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int len1; /* string length for src */
 	int len2; /* string length for dest */
@@ -23,10 +24,11 @@ char *_strcat(char *dest, char *src)
 	while (dest[len2] != '\0')
 		len2++;
 
-	for (i = 0; i <= len1; i++)
-	{
-		dest[len2] = src[i];
-		len2++;
-	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for (; i < n; i++)
+		dest[i] = '\0';
+
 	return (dest);
 }
