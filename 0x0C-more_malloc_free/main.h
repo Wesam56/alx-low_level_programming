b6a111eff4c1e411ba15a6be6e9c3144ca6f4743
Wesam56 <113597907+Wesam56@users.noreply.github.com>
2022-11-11 04:24:04 +0400

    gg

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+void *malloc_checked(unsigned int b);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+
+
+#endif /* MAIN_H */
+ 
